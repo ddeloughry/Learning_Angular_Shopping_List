06cdd9805be59787718085fbc29c56bd98637df9
Daniel Deloughry <ddeloughry@gmail.com>
2019-04-06 17:30:05 +0100

    Add change item on list quantity function

--- controller.js
+++ controller.js
@@ -3,38 +3,65 @@ app.controller("MyController", function($scope){
 	$scope.newItem="";
 	$scope.numNewItems = 1;
 	$scope.myItems = [];
-	$scope.errorText="";
-	$scope.addItem = function(numItems, item){
-		$scope.myItems.push({"num":numItems,"item":item});
+	/**
+	Add an item and its quantity to the list.
+	*/
+	$scope.addItem = function(){
+		$scope.myItems.push({"quantity":$scope.numNewItems,"itemName":$scope.newItem});
 		$scope.newItem="";
 		$scope.numNewItems=1;
 		$scope.errorText="";
 	};
+	/**
+	Remove an item from the list, using its index.
+	*/
 	$scope.removeItem = function(itemIndex){
 		var itemIndex = parseInt(itemIndex,10);
 		$scope.myItems.splice(itemIndex,1);
 	};
-	$scope.changeNumNewItems = function(num){
-		var num = parseInt(num,10);
-		if( ($scope.numNewItems>1) || ($scope.numNewItems==1 && num>-1)){
-			$scope.numNewItems = $scope.numNewItems+num;
+	/**
+	Increment or decrement number of entered items by 1.
+	*/
+	$scope.changeNumNewItems = function(index,num){
+		if(index<0){
+			if( ($scope.numNewItems>1) || ($scope.numNewItems==1 && num>-1)){
+				$scope.numNewItems = $scope.numNewItems+num;
+			}
+		}else{
+			if( ($scope.myItems[index].quantity>1) || ($scope.myItems[index].quantity==1 && num>-1)){
+				$scope.myItems[index].quantity = $scope.myItems[index].quantity+num;
+			}
 		}
 	};
-	$scope.checkNum = function(){
-		if($scope.numNewItems<2){
-			return true;
-		}return false;
+	/**
+	Check if number of entered items is less than 2.
+	Return true if it is.
+	*/
+	$scope.checkNum = function(index){
+		if(index<0){
+			if($scope.numNewItems<2){
+				return true;
+			}return false;
+		}else{
+			if($scope.myItems[index].quantity<2){
+				return true;
+			}return false;
+		}
+		
 	};
+	/**
+	Check if no item has been entered, and if item has already been added to the list.
+	Return true if either have been.
+	*/
 	$scope.validAdd=function(){
-		if($scope.newItem=="" || $scope.alreadyHave()){
+		var valid=false;
+		if($scope.newItem==""){
 			return true;
-		}return false;
-	}
-	$scope.alreadyHave = function(){
+		}
 		for(var i=0;i<$scope.myItems.length;i++){
-			if($scope.myItems[i].item==$scope.newItem){
+			if($scope.myItems[i].itemName==$scope.newItem){
 				return true;
 			}
-		}return false;
-	};
+		}return valid;
+	}
 });
